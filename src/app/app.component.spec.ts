3ae6ba4ae9f6be51064a2b1764c6d7721c876317
Micha Buchholz <mail@lean-stack.de>
2022-11-06 19:55:22 +0100

    Initial static app. Enjoy the ride.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'sample-upvote'`, () => {
+  it(`should have as title 'UpVote'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sample-upvote');
+    expect(app.title).toEqual('UpVote');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('sample-upvote app is running!');
+    expect(compiled.querySelector('h1.title')?.textContent).toContain('UpVote');
   });
 });
---
+++ src/app/seed.ts
@@ -0,0 +1,38 @@
+export const submissions = [
+  {
+    id: 1,
+    title: 'Yellow Pail',
+    description: 'On-demand sand castle construction expertise.',
+    url: '#',
+    votes: 16,
+    avatar: '/assets/img/avatars/daniel.jpg',
+    submissionImage: '/assets/img/submissions/image-yellow.png',
+  },
+  {
+    id: 2,
+    title: 'Supermajority: The Fantasy Congress League',
+    description: 'Earn points when your favorite politicians pass legislation.',
+    url: '#',
+    votes: 11,
+    avatar: '/assets/img/avatars/kristy.png',
+    submissionImage: '/assets/img/submissions/image-rose.png',
+  },
+  {
+    id: 3,
+    title: 'Tinfoild: Tailored tinfoil hats',
+    description: 'We have your measurements and shipping address.',
+    url: '#',
+    votes: 17,
+    avatar: '/assets/img/avatars/veronika.jpg',
+    submissionImage: '/assets/img/submissions/image-steel.png',
+  },
+  {
+    id: 4,
+    title: 'Haught or Naught',
+    description: 'High-minded or absent-minded? You decide.',
+    url: '#',
+    votes: 9,
+    avatar: '/assets/img/avatars/molly.png',
+    submissionImage: '/assets/img/submissions/image-aqua.png',
+  }
+];
